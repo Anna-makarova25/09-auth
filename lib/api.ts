0334ee77a66e2e3522d753e348992167b3274538
Anna Makarova <makarovaanna07@gmail.com>
2026-03-08 00:37:02 +0200

    Refactor baseURL definition in API module for improved flexibility and comment out dynamic export in profile page

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
-
-// const baseURL = 'https://notehub-api.goit.study/api';
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL || ''}/api`;
 
 export const nextServer = axios.create({
   baseURL,
